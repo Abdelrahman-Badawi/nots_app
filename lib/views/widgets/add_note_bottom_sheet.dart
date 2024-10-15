14c9c74239ba7a905ef98406973b304b3d09f29a
Abdelrahman <abdulrahman1badwi@gmail.com>
2024-10-16 01:19:04 +0300

    add field to notes bottom shett

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -6,6 +6,22 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomTextField();
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+      child: Column(
+        children: [
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 7,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,25 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notsapp/constants/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLines=1});
+
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      child: Column(
-        children: [
-          TextField(
-            cursorColor: kPrimaryColor,
-            decoration: InputDecoration(
-              hintText: 'Title',
-              hintStyle: const TextStyle(color: kPrimaryColor),
-              border: buildBorder(),
-              enabledBorder: buildBorder(),
-              focusedBorder: buildBorder(kPrimaryColor),
-            ),
-          )
-        ],
+    return TextField(
+      cursorColor: kPrimaryColor,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
       ),
     );
   }
