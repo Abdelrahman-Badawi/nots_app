b1f71ee846693dda1a014a2bc0dd809d4f5feed3
Abdelrahman <abdulrahman1badwi@gmail.com>
2024-10-20 15:51:28 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,57 +1,69 @@
-
 import 'package:flutter/material.dart';
+import 'package:notsapp/views/edit_note_view.dart';
 
 class NoteIteme extends StatelessWidget {
   const NoteIteme({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 8),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                    color: Colors.black,
-                    fontSize: 26,
-                  ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 8),
-              child: Text(
-                'Build your carear with Abde EL Rahman',
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      color: Colors.black.withOpacity(0.5),
-                      fontSize: 18.0,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 16, bottom: 16, left: 8),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: Colors.black,
+                      fontSize: 26,
                     ),
               ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 35,
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 8),
+                child: Text(
+                  'Build your carear with Abde EL Rahman',
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Colors.black.withOpacity(0.5),
+                        fontSize: 18.0,
+                      ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 35,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'oct14, 2024',
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Colors.black.withOpacity(0.4),
-                  ),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'oct14, 2024',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
